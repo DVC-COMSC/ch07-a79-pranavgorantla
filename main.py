8fb17ecc1560e5611002d09d79326afcafa8f819
pranavgorantla <111912611+pranavgorantla@users.noreply.github.com>
2022-10-24 20:53:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+# Part (1)
+numbers = [ [99,11,66,86,55],[44,21,65,88,24],[33,77,32,33,34]]
 
-numbers = [     [99, 11, 66, 86, 55],
-               [44, 21, 65, 88, 24],
-               [33, 77, 32, 33, 34]]
+# Part (2)
+print("Sum of rows: ",end=" ")
+for i in numbers:
+    print(sum(i),end=" ")
+print(end="\n")
 
+print("Sum of columns: ",end=" ")
+for i in zip(*numbers):
+    print(sum(i),end=" ")
+print(end="\n")
+# Part (3)
+sum_list = []
+for i in numbers:
+    sum_list.append(sum(i))
 
-rnum = len(numbers)
-cnum = len(numbers[0])
+greatest = sum_list.index(max(sum_list))
+print("The row that has greatest sum: ",greatest)
 
-print ('Sum of rows: ', end=' ')
-for i in range(rnum):
-	rsum = sum(numbers[i])
-	print (rsum, end=' ')
-print()
+# Part (4)
+max_list = []
+for i in numbers:
+    max_list.append(max(i))
 
-# ******************************
-# Make your Code
-# ******************************
+print("The greatest number: ",max(max_list))
